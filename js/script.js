0485b3c35e8d8f9b8b3e9621666b0b2cce9e2d4d
Albert Prats <albert.pratspalomares@gmail.com>
2022-08-30 00:12:43 +0200

    Reload

--- js/script.js
+++ js/script.js
@@ -6,17 +6,4 @@ menu.onclick = () => {
     navbar.classList.toggle('active');
 }
 
-//darkmode
-let darkmode = document.querySelector('#darkmode');
-
-darkmode.onclick = () => {
-    if(darkmode.classList.contains('bx-ghost')){
-        darkmode.classList.replace('bx-ghost', 'bx-sun');
-        document.body.classList.add('active');
-    }
-    else{
-        darkmode.classList.replace('bx-sun', 'bx-ghost');
-        document.body.classList.remove('active');
-    }
-}
 
